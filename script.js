84ce3ff22b4a032862fed98d76e4be4b0d79f1af
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2023-04-23 20:15:42 +0530

    init extension

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  const vo = document.querySelector('a[href="/i/verified-orgs-signup"]');
+  const blue = document.querySelector('a[href="/i/twitter_blue_sign_up"]');
+
+  if (vo) vo.remove();
+  if (blue) blue.remove();
+
+}, 2000);
+
